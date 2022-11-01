049f44f2be6fc959187f8a242aa00302baa4b25a
Yung-Hsiang Chiu <mirror.tw@gmail.com>
2022-11-01 14:16:43 +0800

    fix: lint

--- src/client/demo-bootstrap/components/App.jsx
+++ src/client/demo-bootstrap/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Button, Alert } from 'react-bootstrap';
+import { Container, Button, Alert, Form } from 'react-bootstrap';
 import { CSSTransition } from 'react-transition-group';
 
 // This is a wrapper for google.script.run that lets us use promises.
@@ -12,10 +12,7 @@ const App = () => {
   const [showUrl, setShowUrl] = useState();
 
   const getServerUrl = () => {
-    serverFunctions
-      .getServerUrl()
-      .then(setShowUrl)
-      .catch(alert);
+    serverFunctions.getServerUrl().then(setShowUrl).catch(alert);
   };
 
   return (
@@ -52,6 +49,27 @@ const App = () => {
           <Button onClick={() => setShowUrl('')}>Close</Button>
         </Alert>
       </CSSTransition>
+
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We&aposll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </Container>
   );
 };
